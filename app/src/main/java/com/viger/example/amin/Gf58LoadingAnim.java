6a032b9766b7aae590bef6cf904febbc0e58264f
tqw214 <tqw214@163.com>
2017-07-28 12:00:29 +0800

    commit

---
+++ app/src/main/java/com/viger/example/amin/Gf58LoadingAnim.java
@@ -0,0 +1,19 @@
+package com.viger.example.amin;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/7/28.
+ * 仿58同城加载动画效果
+ */
+
+public class Gf58LoadingAnim extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
