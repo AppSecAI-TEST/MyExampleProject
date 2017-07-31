fbab131cd757c58a09bfef4348bc6672d7592f0d
tqw214 <tqw214@163.com>
2017-07-31 10:52:48 +0800

    commit

--- app/src/main/java/com/viger/example/utils/leakcanary/LeakCanaryActivity.java
+++ app/src/main/java/com/viger/example/utils/leakcanary/LeakCanaryActivity.java
@@ -1,9 +1,10 @@
 package com.viger.example.utils.leakcanary;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
 import com.viger.example.R;
 
 /**
@@ -16,6 +17,21 @@ public class LeakCanaryActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leak_canary);
-
     }
+
+    public void startThread(View v) {
+        MyThread myThread = new MyThread();
+        myThread.start();
+    }
+
+    private static class MyThread extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            for(int i=0;i<20;i++) {
+                SystemClock.sleep(1000);
+            }
+        }
+    }
+
 }
